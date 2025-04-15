f7c3600425b0cb157df22099edad749eefb1725b
balakishore-1137 <balakishore.gonga@slu.edu>
2025-04-15 00:54:04 +0000

    updated changes

--- api.js
+++ api.js
@@ -55,17 +55,30 @@ function handleRoot(req, res) {
     res.json(req.body)
   }
 
-  async function deleteProduct(req, res) {
-    const { id } = req.params
-    console.log(`Product with ID ${id} deleted`)
-    res.status(202).json({ message: `Product ${id} deleted.` })
+  async function deleteProduct(req, res, next) {
+    const { id } = req.params;
+    const product = await Products.get(id);
+  
+    if (!product) return next(); // 404 if product not found
+  
+    console.log(`Product with ID ${id} deleted`);
+    res.status(202).json({ message: `Product ${id} deleted.` });
   }
   
-  async function updateProduct(req, res) {
-    const { id } = req.params
-    console.log(`Product with ID ${id} updated`)
-    res.status(200).json({ message: `Product ${id} updated.` })
+  
+  
+  async function updateProduct(req, res, next) {
+    const { id } = req.params;
+    const product = await Products.get(id);
+  
+    if (!product) return next(); // 404 if product not found
+  
+    console.log(`Product with ID ${id} updated`);
+    res.status(200).json({ message: `Product ${id} updated.` });
   }
+  
+  
+  
 
   module.exports = autoCatch({
     handleRoot,
